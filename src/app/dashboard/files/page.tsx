2fad23026b412a677a1e480e403f64eef1b77200
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-09-17 00:33:25 +0500

    some modifications

--- src/app/dashboard/files/page.tsx
+++ src/app/dashboard/files/page.tsx
@@ -1,32 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { FileBrowser } from "../_components/file-browser";
-import { SearchBar } from "../_components/search-bar";
+
 
 export default function FilesPage() {
   return (
     <div className="flex flex-col gap-4">
       {/* Title */}
       <h1 className="text-2xl font-bold">Your Files</h1>
-
-      {/* Row containing SearchBar and Upload Button */}
-      {/* <div className="flex flex-col sm:flex-row items-center sm:items-start gap-4"> */}
-        {/* Search Bar - takes full width except for button */}
-        {/* <div className="w-full sm:flex-grow">
-          <SearchBar query="" setQuery={() => {}} />
-        </div> */}
-
-        {/* Upload Button - aligned to the right */}
-        {/* <div className="w-full sm:w-auto">
-          <Button
-            size="sm"  
-            className="w-full sm:w-auto bg-teal-600"
-          >
-            Upload File
-          </Button>
-        </div> */}
-      {/* </div> */}
 
       {/* File browser below everything */}
       <FileBrowser />
